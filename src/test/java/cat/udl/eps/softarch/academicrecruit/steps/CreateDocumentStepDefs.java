0563a31abeb760874fc571d20c5ac19664d4e6e5
raul <raulac98@gmail.com>
2021-04-14 17:44:31 +0200

    Test of relationships: Document and Selection process tested

--- src/test/java/cat/udl/eps/softarch/academicrecruit/steps/CreateDocumentStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/academicrecruit/steps/CreateDocumentStepDefs.java
@@ -6,32 +6,25 @@ import cat.udl.eps.softarch.academicrecruit.repository.SelectionProcessRepositor
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.springframework.http.MediaType;
-
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 public class CreateDocumentStepDefs {
-
     final StepDefs stepDefs;
     final DocumentRepository documentRepository;
     final SelectionProcessRepository selectionProcessRepository;
     private String newResourceUri;
-
     public CreateDocumentStepDefs(StepDefs stepDefs, DocumentRepository documentRepository, SelectionProcessRepository selectionProcessRepository) {
         this.stepDefs = stepDefs;
         this.documentRepository = documentRepository;
         this.selectionProcessRepository = selectionProcessRepository;
     }
-
-    @When("I create a document with title {string} associated to selection process with vacancy {string}")
-    public void iCreateANewDocumentWithTitle(String title, String vacancy) throws Throwable {
+    @When("I create a document with title {string}")
+    public void iCreateANewDocumentWithTitle(String title) throws Throwable {
         Document document = new Document();
         document.setTitle(title);
-        document.setSelectionProcess(selectionProcessRepository.findByVacancy(vacancy).get(0));
-
         stepDefs.result = stepDefs.mockMvc.perform(
                 post("/documents")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -40,8 +33,7 @@ public class CreateDocumentStepDefs {
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
     }
-
-    @And("It has been created a document with title {string} associated to a selection process with vacancy {string}")
+    @And("It has been created a document with title {string}")
     public void itHasBeenCreatedADocumentWithTitle(String title) throws Throwable {
         newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
         stepDefs.result = stepDefs.mockMvc.perform(
@@ -50,5 +42,28 @@ public class CreateDocumentStepDefs {
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print())
                 .andExpect(jsonPath("$.title", is(title)));
+    }
+    @When("I create a document with title {string} associated to selection process with vacancy {string}")
+    public void iCreateANewDocumentWithTitleAndVacancy(String title, String vacancy) throws Throwable {
+        Document document = new Document();
+        document.setTitle(title);
+        document.setSelectionProcess(selectionProcessRepository.findByVacancy(vacancy).get(0));
+        stepDefs.result = stepDefs.mockMvc.perform(
+                post("/documents")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stepDefs.mapper.writeValueAsString(document))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+    @And("It has been created a document associated to selection process with vacancy {string}")
+    public void itHasBeenCreatedADocumentWithTitleAndVacancy(String vacancy) throws Throwable {
+        newResourceUri = newResourceUri + "/selectionProcess";
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get(newResourceUri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.vacancy", is(vacancy)));
     }
 }

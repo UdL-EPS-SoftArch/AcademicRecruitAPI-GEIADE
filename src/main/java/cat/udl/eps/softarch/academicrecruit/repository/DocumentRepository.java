a8c14b697b4bf3dcf0eba7053b011c25ba4174eb
ivancolomer <31484161+ivancolomer@users.noreply.github.com>
2021-05-12 18:01:53 +0200

    Added paged query for findBySelectionProcess

--- src/main/java/cat/udl/eps/softarch/academicrecruit/repository/DocumentRepository.java
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/repository/DocumentRepository.java
@@ -2,6 +2,8 @@ package cat.udl.eps.softarch.academicrecruit.repository;
 
 import cat.udl.eps.softarch.academicrecruit.domain.Document;
 import cat.udl.eps.softarch.academicrecruit.domain.SelectionProcess;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -20,5 +22,5 @@ public interface DocumentRepository extends PagingAndSortingRepository<Document,
      * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
      */
 
-    List<Document> findBySelectionProcess(@Param("selectionProcess") SelectionProcess selectionProcess);
+    Page<Document> findBySelectionProcess(Pageable pageable, @Param("selectionProcess") SelectionProcess selectionProcess);
 }
